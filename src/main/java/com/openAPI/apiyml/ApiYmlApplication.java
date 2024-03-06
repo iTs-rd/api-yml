89d2a8fc2119d065dd56e53c7c8f6541e1ac356f
Rudresh <rudreshgpt7@gmail.com>
2024-03-06 11:00:03 +0530

    init

---
+++ src/main/java/com/openAPI/apiyml/ApiYmlApplication.java
@@ -0,0 +1,13 @@
+package com.openAPI.apiyml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiYmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openAPI/apiyml/ApiYmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openAPI.apiyml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiYmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
